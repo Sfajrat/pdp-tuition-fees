ea6ae31f6fada11a932b0308696c09f39335384e
Sfajrat <sis.nfs@gmail.com>
2026-03-21 19:03:49 +0300

    interface refactor

--- visualization.py
+++ visualization.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 
 def plot_price_dynamics(df):
-    plt.figure(figsize=(8,5))
+    df = df.sort_values("year")
+    plt.figure(figsize=(8, 5))
     plt.plot(df["year"], df["price"], marker="o")
     plt.xlabel("Год")
     plt.ylabel("Стоимость обучения")
