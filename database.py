0bd4c7144f5875ecb6ed42079fdbd59913676f41
Sfajrat <sis.nfs@gmail.com>
2026-03-26 22:01:59 +0300

    доработано сохранение данных

--- database.py
+++ database.py
@@ -19,6 +19,11 @@ class Database:
         self.conn.commit()
 
     def save_dataframe(self, df):
+        # Важно: если в df нет university — добавляем пустое значение
+        if 'university' not in df.columns:
+            df = df.copy()
+            df['university'] = 'Не указан'
+
         df.to_sql("tuition", self.conn, if_exists="append", index=False)
 
     def load_all(self):
