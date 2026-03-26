fe6b6b9e28370bde53c3f70796cf0ae58bdd31ac
Sfajrat <sis.nfs@gmail.com>
2026-03-26 21:57:31 +0300

    обновлена загрузка файлов

--- data_loader.py
+++ data_loader.py
@@ -2,5 +2,9 @@ import pandas as pd
 
 def load_file(path):
     if path.endswith(".csv"):
-        return pd.read_csv(path)
-    return pd.read_excel(path)
+        df = pd.read_csv(path)
+    elif path.endswith(".xlsx"):
+        df = pd.read_excel(path)
+    else:
+        raise Exception("Неподдерживаемый формат файла")
+    return df
