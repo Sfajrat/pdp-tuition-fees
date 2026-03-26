5f9822220038553255631dfe3f993e9451907c37
Sfajrat <sis.nfs@gmail.com>
2026-03-26 21:59:16 +0300

    обновлено создание таблицы

--- database.py
+++ database.py
@@ -2,15 +2,21 @@ import sqlite3
 import pandas as pd
 
 class Database:
-    def __init__(self, path):
+    def __init__(self, path="tuition.db"):
         self.conn = sqlite3.connect(path)
+
+        # Создаём таблицу
         self.conn.execute("""
             CREATE TABLE IF NOT EXISTS tuition (
                 year INTEGER,
                 program TEXT,
-                price REAL
+                program_length INTEGER,
+                students_count INTEGER,
+                price REAL,
+                university TEXT
             )
         """)
+        self.conn.commit()
 
     def save_dataframe(self, df):
         df.to_sql("tuition", self.conn, if_exists="append", index=False)
