1029b8153d1e756959bbf2c05a19e6d95bacf360
Sfajrat <sis.nfs@gmail.com>
2026-03-26 22:42:02 +0300

    исправлена кодировка

--- export_module.py
+++ export_module.py
@@ -1,12 +1,9 @@
 def export_to_csv(df, path):
-
-    df.to_csv(path, index=False)
+    df.to_csv(path, index=False, encoding='utf-8')
 
 def export_to_excel(df, path):
-
     df.to_excel(path, index=False)
 
 def export_report(report_text, path):
-
     with open(path, "w", encoding="utf-8") as f:
         f.write(report_text)
