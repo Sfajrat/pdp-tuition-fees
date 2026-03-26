306934cff2000c2bd08ae55883497af68cecdc6e
Sfajrat <sis.nfs@gmail.com>
2026-03-26 22:04:19 +0300

    приведено к единообразию использование переменных

--- database.py
+++ database.py
@@ -27,4 +27,5 @@ class Database:
         df.to_sql("tuition", self.conn, if_exists="append", index=False)
 
     def load_all(self):
-        return pd.read_sql("SELECT * FROM tuition", self.conn)
+        df = pd.read_sql("SELECT * FROM tuition", self.conn)
+        return df
