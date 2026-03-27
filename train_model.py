02096e2c8ffd450dc3ce1c5e4d98eda413ca3cb6
Sfajrat <sis.nfs@gmail.com>
2026-03-27 20:53:49 +0300

    в обечение моделей добавлены модели

--- train_model.py
+++ train_model.py
@@ -1,15 +1,16 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-import joblib
+from ml_module import ModelManager
 
+# Загружаем данные и используем только 3 признака, в будущем планируется добавить и другие признаки
 df = pd.read_csv("tuition_data.csv")
 
 X = df[["year", "program_length", "students_count"]]
 y = df["price"]
 
-model = LinearRegression()
-model.fit(X, y)
+manager = ModelManager()
 
-joblib.dump(model, "model.pkl")
+for name in ["linear", "random_forest", "gradient_boosting"]:
+    manager.train_model(name, X, y, save_path=f"model_{name}.pkl")
+    print(f"Модель {name} успешно обучена и сохранена")
 
-print("Модель сохранена")
+print("Все модели переобучены с 3 признаками!")
